126d79cbd3b6da8beb8dc8c2efc83b457f55adb9
samyakshr <124084937+samyakshr@users.noreply.github.com>
2023-02-28 13:26:13 -0500

    Update solar.js

--- solar.js
+++ solar.js
@@ -371,4 +371,31 @@ function makeSumSummaryGraph(names,watts) {
     }
   });
 }
-
+const labels = ["Jan","Feb","Mar","Apr","May","June","July"]
+const site1 = [65, 59, 80, 81, 56, 55, 40];
+const site2 = [24, 34, 54, 62, 34, 54, 23];
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Kilowatt hours by month',
+    data: site1,
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  },{
+    label: 'Kilowatt hours by month',
+    data: site2,
+    fill: false,
+    borderColor: 'rgb(25, 32, 77)',
+    tension: 0.1
+  }]
+};
+ 
+const config = {
+  type: 'line',
+  data: data,
+};
+ 
+const ctx = document.getElementById('myChart');
+  
+new Chart(ctx, config);
