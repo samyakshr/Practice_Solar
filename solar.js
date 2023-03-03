11909008209bac317301661ce073d96548d7dd83
samyakshr <124084937+samyakshr@users.noreply.github.com>
2023-03-03 10:53:57 -0500

    Update solar.js

--- solar.js
+++ solar.js
@@ -209,7 +209,7 @@ function displayAllSiteTodayWatts(data){
 	});
 	console.log(JSON.stringify(names),JSON.stringify(whrs));
 	makeSumSummaryGraph(names,whrs);
-	mkaeSumSummaryLineGraph(names,whrs);
+	makeSumSummaryLineGraph(names,whrs);
 }
 
 // Process All Site watts by hour for that day
@@ -224,7 +224,7 @@ function processAllSiteTodayWatts(results) {
 	//console.log(JSON.stringify(data));
 	document.querySelector('#output2').innerHTML += "<h1>Total Kilowatts today</h1>";
 	displayAllSiteTodayWatts(data);
-	document.querySelector('#output3').innerHTML += "<h1>Total Kilowatts today</h1>";
+	document.querySelector('#output3').innerHTML += "<h1>Line Graph</h1>";
 	displayAllSiteTodayWatts(data);
 }
 
@@ -401,3 +401,4 @@ function makeSumSummaryLineGraph(names,watts) {
     }
   });
 }
+
